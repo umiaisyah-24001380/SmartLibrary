f69e534d8585c82dd89b987f7cd623e9ac90fefb
IZZATUL IMAN <24001093@siswa.um.edu.my>
2026-06-04 09:34:16 +0800

    Update and rename ILibrarySystem.java to LibraryADT.java

--- src/LibraryADT.java
+++ src/LibraryADT.java
@@ -1,10 +1,10 @@
 package smartlibrary;
 
 //The Console Interface
-public interface ILibrarySystem {
+public interface LibraryADT {
     void addBook (int isbn , String title , String author); // Add a new book to the library catalogue
     void searchBook (int isbn); // Search the book by ISBN using BST
     void borrowBook (int isbn); // Borrow a book and move record to history stack (LIFO)
-    void displayHistory (); // Display all borrowed books in LIFO order
+    void viewLatestHistory (); // Display all borrowed books in LIFO order
     
 }
